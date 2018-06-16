fe97aa3d8babe15cfb35510b9d6b47e733b3e4f4
Brian Long <briantlong29@gmail.com>
2018-06-16 00:02:53 -0700

    Update Event_SelectByFollowingUserId.sql

--- Event_SelectByFollowingUserId.sql
+++ Event_SelectByFollowingUserId.sql
@@ -13,16 +13,15 @@ GO
 ALTER PROCEDURE [dbo].[Events_ProspectEvent_SelectByFollowingUserId]
 	-- Add the parameters for the stored procedure here
   
-	@UserBaseId int 
-	--@Id int
+@UserBaseId int 
+--@Id int
   
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
+SET NOCOUNT ON;
+
+
     
   /* -------------------- Test ------------------------------
 
@@ -35,76 +34,76 @@ BEGIN
 
 	exec dbo.Users_UserBase_SelectAll
 
-	*/ ---------------------------------------------------------
+*/ ---------------------------------------------------------
   
-	SELECT 
-		CONCAT(a.City, ', ', sp.StateProvinceCode) as cityState,
-		CONCAT(a.StreetAddress, ', ', a.City) as streetCity,
-		p.CreatedById as EventCreator,
-		up.AvatarUrl,
-		CONCAT(up.FirstName, ' ', up.LastName) as FullName,
-		p.Id, 
-		p.EventName, 
-		et.TypeName as EventTypeName,
-		et.Id as EventTypeId,
-		p.EventDescription,
-		CONCAT(a.StreetAddress, ' ', a.City, ',', sp.StateProvinceCode, ' ', a.PostalCode) as fullAddress,
-		a.StreetAddress,
-		a.City,
-		sp.StateProvinceCode,
-		sp.Id as StateProvinceId,
-		p.AddressId,
-		a.PostalCode,
-		p.StartDate,
-		p.EndDate,
-		p.StartTime,
-		p.EndTime,
-		p.IsAllDayEvent,
-		p.CanRepeat,
-		p.TicketPrice,
-		p.PhotoUrl,
-		p.ExternalSiteUrl,
-		p.IsAdminApproved,
-		p.CreatedDate,
-		p.CreatedById,
-		p.ModifiedDate,
-		p.ModifiedById,
-		ea.UserBaseId,
-		ea.Attendance
+SELECT 
+	CONCAT(a.City, ', ', sp.StateProvinceCode) as cityState,
+	CONCAT(a.StreetAddress, ', ', a.City) as streetCity,
+	p.CreatedById as EventCreator,
+	up.AvatarUrl,
+	CONCAT(up.FirstName, ' ', up.LastName) as FullName,
+	p.Id, 
+	p.EventName, 
+	et.TypeName as EventTypeName,
+	et.Id as EventTypeId,
+	p.EventDescription,
+	CONCAT(a.StreetAddress, ' ', a.City, ',', sp.StateProvinceCode, ' ', a.PostalCode) as fullAddress,
+	a.StreetAddress,
+	a.City,
+	sp.StateProvinceCode,
+	sp.Id as StateProvinceId,
+	p.AddressId,
+	a.PostalCode,
+	p.StartDate,
+	p.EndDate,
+	p.StartTime,
+	p.EndTime,
+	p.IsAllDayEvent,
+	p.CanRepeat,
+	p.TicketPrice,
+	p.PhotoUrl,
+	p.ExternalSiteUrl,
+	p.IsAdminApproved,
+	p.CreatedDate,
+	p.CreatedById,
+	p.ModifiedDate,
+	p.ModifiedById,
+	ea.UserBaseId,
+	ea.Attendance
 
 
-	FROM dbo.Events_ProspectEvent as p
+FROM dbo.Events_ProspectEvent as p
 
-	inner join dbo.Address_Address as a
+inner join dbo.Address_Address as a
 
-	on a.id = p.AddressId
+on a.id = p.AddressId
 
-	inner join dbo.StateProvince as sp
+inner join dbo.StateProvince as sp
 
-	on sp.Id = a.StateProvinceId
+on sp.Id = a.StateProvinceId
 
-	inner join dbo.Events_EventType as et
+inner join dbo.Events_EventType as et
 
-	on et.Id = p.EventTypeId
+on et.Id = p.EventTypeId
 
-	inner join dbo.Address_AddressType as atype
+inner join dbo.Address_AddressType as atype
 
-	on atype.Id = a.AddressTypeId
+on atype.Id = a.AddressTypeId
 
-	inner join dbo.Users_UserProfile as up
+inner join dbo.Users_UserProfile as up
 
-	on up.UserBaseId = p.CreatedById
+on up.UserBaseId = p.CreatedById
 
-	inner join dbo.Events_EventAttendance as ea
+inner join dbo.Events_EventAttendance as ea
 
-	on ea.EventId = p.Id
+on ea.EventId = p.Id
 
-	and ea.UserBaseId=@UserBaseId
+and ea.UserBaseId=@UserBaseId
 
-	where (ea.Attendance = 'Yes' or ea.Attendance = 'Maybe') and p.EndDate >= GETDATE()
+where (ea.Attendance = 'Yes' or ea.Attendance = 'Maybe') and p.EndDate >= GETDATE()
 
-	ORDER BY
-	p.StartDate ASC
+ORDER BY
+p.StartDate ASC
 
 	
 END
